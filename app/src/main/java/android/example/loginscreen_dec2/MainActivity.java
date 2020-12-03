2ec1001b836d8b2972a04fd42ff0e6f4a022ca36
raju <raju.imp25@gmail.com>
2020-12-03 22:16:26 +0530

    Login and Registration

--- app/src/main/java/android/example/loginscreen_dec2/MainActivity.java
+++ app/src/main/java/android/example/loginscreen_dec2/MainActivity.java
@@ -1,14 +1,39 @@
 package android.example.loginscreen_dec2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView RG=(TextView) findViewById(R.id.Register);
+        CardView CV = (CardView) findViewById(R.id.CardView);
+
+        RG.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Register.class);
+                startActivity(intent);
+            }
+        });
+
+        CV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"clicked",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+
+
 }
